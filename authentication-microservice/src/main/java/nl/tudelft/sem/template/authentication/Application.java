fa66708e97c9795c54cafdec617791f942e12a3b
Vlad Alexan <v.alexan@student.tudelft.nl>
2023-11-22 23:57:50 +0100

    Add Book entity and update AppUser entity.

--- authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/Application.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/Application.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class Application {
-    /**
-     * Main class
-     *
-     * @param args String array input
-     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
