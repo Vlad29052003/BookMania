b0dfc97f85dcd0ba0b17273a49bf16a40f2fce92
Simona Cristina Lupsa <s.c.lupsa@student.tudelft.nl>
2023-12-07 20:54:35 +0100

    tests

---
+++ users-microservice/src/test/java/nl/tudelft/sem/template/authentication/controller/ReportControllerTests.java
@@ -0,0 +1,66 @@
+package nl.tudelft.sem.template.authentication.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import nl.tudelft.sem.template.authentication.controllers.ReportController;
+import nl.tudelft.sem.template.authentication.domain.report.Report;
+import nl.tudelft.sem.template.authentication.domain.report.ReportService;
+import nl.tudelft.sem.template.authentication.domain.report.ReportType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ReportControllerTests {
+
+    private transient ReportService reportService;
+    private transient ReportController reportController;
+    private transient Report report;
+    private transient UUID repordId;
+    private transient String token;
+
+    @BeforeEach
+    public void setUp() {
+        this.reportService = mock(ReportService.class);
+        this.reportController = new ReportController(reportService);
+        this.report = new Report(ReportType.REVIEW, UUID.randomUUID().toString(), "Report");
+        this.repordId = UUID.randomUUID();
+        this.token = "someToken";
+    }
+
+    @Test
+    public void getAllReportsTest() {
+        when(reportService.getAllReports(token)).thenReturn(List.of(report));
+        assertEquals(reportController.getAllReports(token), ResponseEntity.ok(List.of(report)));
+
+        when(reportService.getAllReports(token)).thenThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Only admins can access reports!"));
+        assertEquals(reportController.getAllReports(token).getStatusCodeValue(), 401);
+    }
+
+    @Test
+    public void addReportTest() {
+        assertEquals(reportController.addReport(report, token).getStatusCodeValue(), 200);
+
+        doThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token!")).when(reportService).addReport(any(), any());
+        assertEquals(reportController.addReport(report, token).getStatusCodeValue(), 401);
+
+        doThrow(new ResponseStatusException((HttpStatus.NOT_FOUND), "Reported user not found!")).when(reportService).addReport(any(), any());
+        assertEquals(reportController.addReport(report, token).getStatusCodeValue(), 404);
+    }
+
+    @Test
+    public void deleteReportTest() {
+        assertEquals(reportController.deleteReport(repordId.toString(), token).getStatusCodeValue(), 200);
+
+        doThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Only admins can access reports!")).when(reportService).remove(any(), any());
+        assertEquals(reportController.deleteReport(repordId.toString(), token).getStatusCodeValue(), 401);
+
+        doThrow(new ResponseStatusException((HttpStatus.NOT_FOUND), "The report does not exist!")).when(reportService).remove(any(), any());
+        assertEquals(reportController.deleteReport(repordId.toString(), token).getStatusCodeValue(), 404);
+    }
+}
---
+++ users-microservice/src/test/java/nl/tudelft/sem/template/authentication/domain/report/ReportServiceTests.java
@@ -0,0 +1,99 @@
+package nl.tudelft.sem.template.authentication.domain.report;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import nl.tudelft.sem.template.authentication.authentication.JwtService;
+import nl.tudelft.sem.template.authentication.domain.user.Authority;
+import nl.tudelft.sem.template.authentication.domain.user.UserRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
+class ReportServiceTests {
+
+    @Mock
+    private ReportRepository reportRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private JwtService jwtService;
+    @InjectMocks
+    private ReportService reportService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void getAllReportsGoodTest() {
+        when(jwtService.extractAuthorization(anyString())).thenReturn(Authority.ADMIN);
+        when(reportRepository.findAll()).thenReturn(Collections.emptyList());
+        assertDoesNotThrow(() -> reportService.getAllReports("Admin token"));
+        verify(reportRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void getAllReportsNotAdminTest() {
+        when(jwtService.extractAuthorization(anyString())).thenReturn(Authority.REGULAR_USER);
+        assertThrows(ResponseStatusException.class, () -> reportService.getAllReports("User token"));
+    }
+
+    @Test
+    public void getAllReportsExpiredTest() {
+        when(jwtService.isTokenExpired(anyString())).thenReturn(true);
+        when(jwtService.extractAuthorization(anyString())).thenReturn(Authority.REGULAR_USER);
+        assertThrows(ResponseStatusException.class, () -> reportService.getAllReports("Expired token"));
+    }
+
+    @Test
+    void addReportGoodTest() {
+        when(jwtService.isTokenExpired(anyString())).thenReturn(false);
+        when(userRepository.existsById(any())).thenReturn(true);
+        Report report = new Report(ReportType.REVIEW, UUID.randomUUID().toString(), "Some language");
+        assertDoesNotThrow(() -> reportService.addReport(report, "User token"));
+        verify(reportRepository, times(1)).saveAndFlush(any());
+    }
+
+    @Test
+    void addReportExpiredTest() {
+        when(jwtService.isTokenExpired(anyString())).thenReturn(true);
+        Report report = new Report(ReportType.REVIEW, UUID.randomUUID().toString(), "Some language");
+        assertThrows(ResponseStatusException.class, () -> reportService.addReport(report, "Expired token"));
+    }
+
+    @Test
+    void removeGoodTest() {
+        when(jwtService.extractAuthorization(anyString())).thenReturn(Authority.ADMIN);
+        UUID reportId = UUID.randomUUID();
+        when(reportRepository.findById(reportId)).thenReturn(Optional.of(new Report()));
+        assertDoesNotThrow(() -> reportService.remove(reportId.toString(), "Admin token"));
+        verify(reportRepository, times(1)).deleteById(reportId);
+    }
+    @Test
+    void removeNotFoundTest() {
+        when(jwtService.extractAuthorization(anyString())).thenReturn(Authority.ADMIN);
+        UUID reportId = UUID.randomUUID();
+        when(reportRepository.findById(reportId)).thenReturn(Optional.empty());
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
+                () -> reportService.remove(reportId.toString(), "Admin token"));
+        assert(exception.getStatus() == HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void removeNotAdminTest() {
+        when(jwtService.extractAuthorization(anyString())).thenReturn(Authority.REGULAR_USER);
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
+                () -> reportService.remove(UUID.randomUUID().toString(), "User token"));
+        assert(exception.getStatus() == HttpStatus.UNAUTHORIZED);
+    }
+
+}
