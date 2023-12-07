3811597c6d7dec0b5aceedff38b0154f9fcc12c2
Andrei Stefan <A.Stefan-1@student.tudelft.nl>
2023-12-07 12:25:10 +0100

    Adapted to new merges on main branch.

---
+++ users-microservice/src/main/java/nl/tudelft/sem/template/authentication/controllers/UserSearchController.java
@@ -0,0 +1,56 @@
+package nl.tudelft.sem.template.authentication.controllers;
+
+//import nl.tudelft.sem.template.authentication.authentication.JwtUserDetailsService;
+import nl.tudelft.sem.template.authentication.domain.user.AppUser;
+import nl.tudelft.sem.template.authentication.domain.user.UserLookupService;
+import nl.tudelft.sem.template.authentication.models.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/c")
+public class UserSearchController {
+
+
+    //private final transient JwtUserDetailsService jwtUserDetailsService;
+
+    private final transient UserLookupService userLookupService;
+
+    /**
+     * Instantiates a new UsersController.
+     * //@param jwtUserDetailsService the user service
+     *
+     * @param lookupService         the user lookup service
+     */
+    @Autowired
+    public UserSearchController(UserLookupService lookupService) {
+
+        //this.jwtUserDetailsService = jwtUserDetailsService;
+        this.userLookupService = lookupService;
+    }
+
+    /**
+     * Endpoint for user searching.
+     *
+     * @param userName The username query parameter
+     * @return the users containing the given username
+     * @throws ResponseStatusException if the user does not exist or the password is incorrect
+     */
+    @GetMapping("/users/{user}")
+    public ResponseEntity<Iterable<UserModel>> getUser(@PathVariable(name = "user") String userName)
+            throws ResponseStatusException {
+        try {
+            Iterable<UserModel> x = userLookupService.getUsersByName(userName);
+            //System.out.println(x);
+            return ResponseEntity.ok(x);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+
+}
+
---
+++ users-microservice/src/test/java/nl/tudelft/sem/template/authentication/domain/user/UserLookupServiceTests.java
@@ -0,0 +1,146 @@
+package nl.tudelft.sem.template.authentication.domain.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import nl.tudelft.sem.template.authentication.authentication.JwtService;
+import nl.tudelft.sem.template.authentication.authentication.JwtTokenGenerator;
+import nl.tudelft.sem.template.authentication.authentication.JwtUserDetailsService;
+import nl.tudelft.sem.template.authentication.models.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+// activate profiles to have spring use mocks during auto-injection of certain beans.
+@ActiveProfiles({"test", "mockPasswordEncoder"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class UserLookupServiceTests {
+
+    @Autowired
+    private transient UserLookupService userLookupService;
+
+    @Autowired
+    private transient AuthenticationService authenticationService;
+
+    @Autowired
+    private transient UserRepository userRepository;
+
+    @Autowired
+    private transient AuthenticationManager authenticationManager;
+
+    @Autowired
+    private transient JwtTokenGenerator jwtTokenGenerator;
+
+    @Autowired
+    private transient JwtUserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private transient JwtService jwtService;
+    private transient RegistrationRequestModel registrationRequest;
+    private transient RegistrationRequestModel registrationRequest2;
+
+
+    /**
+     * Sets up the testing environment.
+     */
+    @BeforeEach
+    public void setUp() {
+        PasswordHashingService passwordHashingService = mock(PasswordHashingService.class);
+        when(passwordHashingService.hash(new Password("someOtherHash"))).thenReturn(new HashedPassword("someHash"));
+        authenticationService = new AuthenticationService(authenticationManager,
+                jwtTokenGenerator, jwtUserDetailsService,
+                jwtService, userRepository, passwordHashingService);
+
+        String email = "email";
+        String netId = "user";
+        String password = "someHash";
+        UUID id = UUID.randomUUID();
+
+        AppUser appUser = new AppUser(new Username(netId), email, new HashedPassword(password));
+        appUser.setId(id);
+
+        registrationRequest = new RegistrationRequestModel();
+        registrationRequest.setUsername(netId);
+        registrationRequest.setEmail(email);
+        registrationRequest.setPassword(password);
+
+        AuthenticationRequestModel authenticationRequest = new AuthenticationRequestModel();
+        authenticationRequest.setPassword(password);
+        authenticationRequest.setUsername(email);
+
+        AuthenticationResponseModel authenticationResponse = new AuthenticationResponseModel();
+        String token = "Bearer token";
+        authenticationResponse.setToken(token);
+
+        TokenValidationResponse tokenValidationResponse = new TokenValidationResponse();
+        tokenValidationResponse.setId(id);
+
+
+        String email2 = "email2";
+        String netId2 = "andrei";
+        String password2 = "someHash";
+        UUID id2 = UUID.randomUUID();
+
+        AppUser appUser2 = new AppUser(new Username(netId2), email2, new HashedPassword(password2));
+        appUser2.setId(id2);
+
+        registrationRequest2 = new RegistrationRequestModel();
+        registrationRequest2.setUsername(netId2);
+        registrationRequest2.setEmail(email2);
+        registrationRequest2.setPassword(password2);
+
+    }
+
+    /**
+     * Tests whether the user is correctly returned when using the proper query string.
+     *
+     */
+    @Test
+    public void userSearchByName_worksCorrectly() {
+
+        authenticationService.registerUser(registrationRequest);
+        authenticationService.registerUser(registrationRequest2);
+
+
+        // Assert
+        List<String> foundUsers = userLookupService.getUsersByName("user")
+                .stream().map(UserModel::getNetId).collect(Collectors.toList());
+        List<String> expected = List.of("user");
+
+
+        assertThat(foundUsers).containsAll(expected);
+    }
+
+
+    /**
+     * Tests whether all users are returned when using an empty query string.
+     *
+     */
+    @Test
+    public void userSearchByName_worksCorrectly2() {
+
+        authenticationService.registerUser(registrationRequest);
+        authenticationService.registerUser(registrationRequest2);
+
+
+        // Assert
+        List<String> foundUsers = userLookupService.getUsersByName("")
+                .stream().map(UserModel::getNetId).collect(Collectors.toList());
+        List<String> expected = List.of("user", "andrei");
+
+        assertThat(foundUsers).containsAll(expected);
+    }
+}
