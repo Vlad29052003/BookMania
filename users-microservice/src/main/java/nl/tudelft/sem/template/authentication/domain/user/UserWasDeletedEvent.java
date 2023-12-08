acbd049c67b507eaed07716cc4e056a3fac7d9e6
Charlene Kay Bakker <c.k.bakker@student.tudelft.nl>
2023-12-08 11:54:48 +0100

    Added a UserWasDeletedEvent class

--- users-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/UserWasDeletedEvent.java
+++ users-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/UserWasDeletedEvent.java
@@ -1,2 +1,12 @@
-package nl.tudelft.sem.template.authentication.domain.user;public class UserWasDeletedEvent {
+package nl.tudelft.sem.template.authentication.domain.user;
+
+import lombok.Getter;
+
+@Getter
+public class UserWasDeletedEvent {
+    private final transient Username username;
+
+    public UserWasDeletedEvent(Username username) {
+        this.username = username;
+    }
 }
