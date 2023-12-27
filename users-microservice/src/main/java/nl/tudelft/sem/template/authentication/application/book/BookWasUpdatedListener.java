c1f409076c2697f9969f7199424558d0003f705a
pmeswani <p.m.meswani@student.tudelft.nl>
2023-12-27 15:53:12 +0100

    Changes to listener

--- users-microservice/src/main/java/nl/tudelft/sem/template/authentication/application/book/BookWasUpdatedListener.java
+++ users-microservice/src/main/java/nl/tudelft/sem/template/authentication/application/book/BookWasUpdatedListener.java
@@ -5,10 +5,13 @@ import nl.tudelft.sem.template.authentication.domain.book.BookWasCreatedEvent;
 import nl.tudelft.sem.template.authentication.domain.book.BookWasDeletedEvent;
 import nl.tudelft.sem.template.authentication.domain.book.BookWasEditedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  * This event listener is automatically called when a domain entity is saved
@@ -17,23 +20,34 @@ import reactor.core.publisher.Mono;
 @Component
 public class BookWasUpdatedListener {
 
-    private static final String bookshelfUri = "http://localhost:8081/a";
-    private static final WebClient wc = WebClient.create(bookshelfUri);
+    private static final String bookshelfUri = "http://localhost:8081/a/catalog";
+    private static final HttpClient client = HttpClient.newHttpClient();
 
     /**
      * Event handler for creating books.
      *
      * @param event The event to react to
+     * @throws RuntimeException in the case that the request is not sent correctly
      */
     @EventListener
     public void onBookWasCreated(BookWasCreatedEvent event) {
         // Handler code here
         Book book = event.getBook();
 
-        System.out.println("Book (" + book.getTitle() + ") was created.");
 
-        wc.post().uri("/catalog").body(Mono.just(book), Book.class)
-                .accept(MediaType.APPLICATION_JSON);
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(bookshelfUri))
+                    .PUT(HttpRequest.BodyPublishers.noBody())
+                    .build();
+
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Book (" + book.getTitle() + ") was created.");
 
     }
 
@@ -41,6 +55,7 @@ public class BookWasUpdatedListener {
      * Event handler for deleting books.
      *
      * @param event The event to react to
+     * @throws RuntimeException in the case that the request is not sent correctly
      */
     @EventListener
     public void onBookWasDeleted(BookWasDeletedEvent event) {
@@ -48,21 +63,39 @@ public class BookWasUpdatedListener {
 
         System.out.println("Book (" + book.getTitle() + ") was deleted.");
 
-        wc.delete().uri("/catalog?bookId=" + book.getId());
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(bookshelfUri + "?bookId=" + book.getId()))
+                    .PUT(HttpRequest.BodyPublishers.noBody())
+                    .build();
+
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
      * Event handler for editing books.
      *
      * @param event The event to react to
+     * @throws RuntimeException in the case that the request is not sent correctly
      */
     @EventListener
     public void onBookWasEdited(BookWasEditedEvent event) {
         Book book = event.getBook();
 
-        System.out.println("Book (" + book.getTitle() + ") was edited.");
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(bookshelfUri))
+                    .POST(HttpRequest.BodyPublishers.noBody())
+                    .build();
 
-        wc.put().uri("/catalog").body(Mono.just(book), Book.class)
-                .accept(MediaType.APPLICATION_JSON);
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Book (" + book.getTitle() + ") was edited.");
     }
 }
--- users-microservice/src/test/java/nl/tudelft/sem/template/authentication/domain/book/BookWasUpdatedListenerTests.java
+++ users-microservice/src/test/java/nl/tudelft/sem/template/authentication/domain/book/BookWasUpdatedListenerTests.java
@@ -7,66 +7,136 @@ import nl.tudelft.sem.template.authentication.application.book.BookWasUpdatedLis
 import nl.tudelft.sem.template.authentication.domain.book.BookWasCreatedEvent;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
+import wiremock.org.apache.hc.core5.http.HttpEntity;
+import wiremock.org.apache.hc.core5.http.io.entity.EntityUtils;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@AutoConfigureMockMvc
 public class BookWasUpdatedListenerTests {
-        @Autowired
-        private transient MockMvc mockMvc;
 
-        @Autowired
-        private transient BookWasUpdatedListener bookWasUpdatedListener;
+    private transient BookWasUpdatedListener bookWasUpdatedListener = new BookWasUpdatedListener();
 
-        private static WireMockServer bookshelfServer;
+    private static WireMockServer bookshelfServer;
 
-        private transient ByteArrayOutputStream outputStreamCaptor;
+    private static ByteArrayOutputStream outputStreamCaptor;
 
-        @BeforeAll
-        static void init() {
-            bookshelfServer = new WireMockServer(
-                    new WireMockConfiguration().port(8081)
-            );
-            bookshelfServer.start();
-            WireMock.configureFor("localhost", 8081);
-        }
+    /**
+     * Set up before all tests.
+     */
+    @BeforeAll
+    static void init() {
+        //bookWasUpdatedListener = new BookWasUpdatedListener();
 
-        @Test
-        public void testPost() {
-            stubFor(post(urlEqualTo("/a/catalog")).willReturn(aResponse().withStatus(200)));
-            stubFor(put(urlEqualTo("/a/catalog")).willReturn(aResponse().withStatus(200)));
-            stubFor(delete(urlEqualTo("/a/catalog")).willReturn(aResponse().withStatus(200)));
+        bookshelfServer = new WireMockServer(
+                new WireMockConfiguration().port(8081)
+        );
+        bookshelfServer.start();
+        WireMock.configureFor("localhost", 8081);
 
-            Book book = new Book("title", null, null, "", 1);
+        stubFor(post(urlEqualTo("/a/catalog")).willReturn(aResponse().withStatus(200)));
+        stubFor(put(urlEqualTo("/a/catalog")).willReturn(aResponse().withStatus(200)));
+        stubFor(delete(urlEqualTo("/a/catalog")).willReturn(aResponse().withStatus(200)));
 
-            bookWasUpdatedListener.onBookWasCreated(new BookWasCreatedEvent(book));
+        outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
 
-            verify(putRequestedFor(urlPathEqualTo("/a/catalog")));
+//    @Test
+//    public void testStubs() throws IOException {
+//        HttpClient client = HttpClient.newHttpClient();
+//
+//        try {
+//            HttpRequest request = HttpRequest.newBuilder()
+//                    .uri(URI.create("http://localhost:8081/a/catalog"))
+//                    .PUT(HttpRequest.BodyPublishers.noBody())
+//                    .build();
+//
+//            client.send(request, HttpResponse.BodyHandlers.ofString());
+//
+//            verify(putRequestedFor(urlPathEqualTo("/a/catalog")));
+//
+//            HttpRequest request2 = HttpRequest.newBuilder()
+//                    .uri(URI.create("http://localhost:8081/a/catalog"))
+//                    .POST(HttpRequest.BodyPublishers.noBody())
+//                    .build();
+//
+//            client.send(request2, HttpResponse.BodyHandlers.ofString());
+//
+//            verify(postRequestedFor(urlPathEqualTo("/a/catalog")));
+//
+//            HttpRequest request3 = HttpRequest.newBuilder()
+//                    .uri(URI.create("http://localhost:8081/a/catalog"))
+//                    .DELETE()
+//                    .build();
+//
+//            client.send(request3, HttpResponse.BodyHandlers.ofString());
+//
+//            verify(deleteRequestedFor(urlPathEqualTo("/a/catalog")));
+//        } catch (IOException | InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
 
-            assertThat(outputStreamCaptor.toString().trim())
-                    .isEqualTo("Book (" + book.getTitle() + ") was created.");
-        }
+    @Test
+    public void testPut() {
 
-        /**
-         * Set up for the testing environment after each test.
-         */
-        @AfterEach
-        public void afterEach() {
-            bookshelfServer.stop();
-        }
+        Book book = new Book("title", null, null, "", 1);
+
+        bookWasUpdatedListener.onBookWasCreated(new BookWasCreatedEvent(book));
+
+        assertThat(outputStreamCaptor.toString().trim())
+                .contains("Book (" + book.getTitle() + ") was created.");
+
+        verify(putRequestedFor(urlPathEqualTo("/a/catalog")));
+
+    }
+
+    @Test
+    public void testPost() {
+        Book book = new Book("title", null, null, "", 1);
+
+        bookWasUpdatedListener.onBookWasEdited(new BookWasEditedEvent(book));
+
+        assertThat(outputStreamCaptor.toString().trim())
+                .contains("Book (" + book.getTitle() + ") was edited.");
+
+        verify(postRequestedFor(urlPathEqualTo("/a/catalog")));
+    }
+
+    /**
+     * Set up for the testing environment after each test.
+     */
+    @AfterEach
+    public void afterEach() {
+        bookshelfServer.stop();
+    }
 }
