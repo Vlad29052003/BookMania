452f69dfac572cc4b06bcaed691691a4e9a3c25b
Simona Cristina Lupsa <s.c.lupsa@student.tudelft.nl>
2024-01-11 21:08:25 +0100

    change admin script

--- users-microservice/src/main/resources/data.sql
+++ users-microservice/src/main/resources/data.sql
@@ -1,11 +1,12 @@
 DELETE FROM USERS WHERE id = '6ba7b811-9dad-11d1-80b4-00c04fd430c8';
-INSERT INTO USERS (id, username, email, password_hash, authority, deactivated, private)
-    VALUES ('6ba7b811-9dad-11d1-80b4-00c04fd430c8',
-            'PrimeAdmin',
-            'admin@email.com',
-            '$2a$10$gLOuGk9WZgRVgTto/AVe1uPRmPOSMblfcj4I1NjAQrV/Q6XWCiM8e',
-            'ADMIN',
-            false,
-            true);
+INSERT INTO USERS (id, username, email, password_hash, authority, deactivated, private, two_factor_auth)
+VALUES ('6ba7b811-9dad-11d1-80b4-00c04fd430c8',
+        'PrimeAdmin',
+        'admin@email.com',
+        '$2a$10$gLOuGk9WZgRVgTto/AVe1uPRmPOSMblfcj4I1NjAQrV/Q6XWCiM8e',
+        'ADMIN',
+        false,
+        true,
+        false);
 
 -- username: PrimeAdmin, password: Pass123!
